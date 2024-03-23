ceb9da0fb5632db7fe25d752fafaf4d6091f79df
StealthM0de <jedongfallorina@gmail.com>
2024-03-23 17:23:27 +0800

    update portfolio

--- src/data/social-links.jsx
+++ src/data/social-links.jsx
@@ -5,17 +5,17 @@ import { IoIosMail } from 'react-icons/io';
 export const socialLinks = [
   {
     id: 1,
-    href: 'https://facebook.com/marventures11',
+    href: 'https://www.facebook.com/jed.fallorina/',
     icon: FaFacebookF,
   },
   {
     id: 2,
-    href: 'https://linkedin.com/in/marventures',
+    href: 'https://www.linkedin.com/in/jed-alston-fallorina-992853254/',
     icon: FaLinkedinIn,
   },
   {
     id: 3,
-    href: 'https://github.com/marventures',
+    href: 'https://github.com/StealthM0de',
     icon: FaGithubAlt,
   },
   {
